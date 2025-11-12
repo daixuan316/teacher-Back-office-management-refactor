eb1df654c7f1ed9122c8d201a9e0d7fe30ae86bd
王大帆 <9132017+wang-da-fan@user.noreply.gitee.com>
2025-11-13 00:21:06 +0800

    feat:导入初步的库，初步配置

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './assets/main.css'
-
+import PrimeVue from 'primevue/config';
 import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+app.use(PrimeVue, { ripple: true });
