30f800e983e68265f45e7b15714928c1bfafa5f3
Shawn.D <shawn.d@q-code.co>
2025-11-13 09:44:09 +0800

    feat:登录文件搭建

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
 const routes = [
+  {
+    path: "/",
+    name: "login",
+    component: () => import("@/pages/login/index.vue"),
+  },
 ];
 const router = createRouter({
-   history: createWebHistory(import.meta.env.BASE_URL),
-   routes,
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
 });
 export default router;
